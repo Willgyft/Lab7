6ca1c3b6b97a04a967d5b4b814a696dd5211e710
Willgyft <warintorn_sr@cmu.ac.th>
2025-12-16 14:35:39 +0700

    lab7_2.cpp complete

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -10,25 +10,26 @@ int main(){
     string signature;
     cout << "Fahsai: Sawadee ka...Can you tell me your name?\n";
     getline(cin, name);
-    cout << "?????: " << name << endl;
+    cout << "?????: ";
     cout << "Fahsai: Wow!!! " << name << " is a really cool name.\n";
     cout << "Fahsai: I think you are an Engineering student. What is your student ID?\n";
     cin >> number;
     int gear = number/10000000 ;
-    cout << name << ": " << number << endl;
+    cout << name << ": ";
     cout << "Fahsai: I think you may be GEAR " << gear - 12 << ". I have a free movie ticket for you.\n";
     cout << "Fahsai: Let's go to the cinema together!!!\n";
     cout << "Fahsai: What movie do you want to watch?\n";
     cin.ignore();
     getline(cin,movie);
-    cout << name << ": " << movie << endl;
+    cout << name << ": ";
     cout << "Fahsai: So....which day are you free to go with me?\n";
     getline(cin,day);
     cout << name << ": ";
     cout << "Fahsai: " << day << "....that is OK!!! I'm looking forward to watching " << movie << " with you.\n";
     getline(cin,signature);
-    cout << name << ": " << signature << endl;
-    cout << "Fahsai: 555+ see you" << day << ". Bye Bye \\(^ ^)/";
-    return 0;
+    cout << name << ": ";
+    cout << "Fahsai: 555+ see you " << day << ". Bye Bye \\(^ ^)/";
+
 }
+
 
